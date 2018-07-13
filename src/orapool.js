a88475b434d101f2f68caa891d7ab047a6af4921
JulienF <j.fouquet@bimedia.com.fr>
2018-07-13 12:58:21 +0200

    Fixed getConnection, connection (conn) and config for schema

--- src/orapool.js
+++ src/orapool.js
@@ -10,20 +10,19 @@ function deferred(fn) {
     return str;
 }
 
-function Pool(orapool){
+function Pool(orapool, config){
     this._pool = orapool;
+    this._config = config;
 }
 
-Pool.prototype.getConnection = function () {
-    return this._pool.getConnection.apply(this._pool, arguments);
-};
-
 Pool.prototype.query = function (sql, binding, options) {
-    return this.getConnection().then(conn => {
-        var p = config.schema ? connection.execute('ALTER SESSION SET CURRENT_SCHEMA = ' + config.schema) : Promise.resolve();
+    return this._pool.getConnection().then(conn => {
+        var p = this._config.schema ? conn.execute('ALTER SESSION SET CURRENT_SCHEMA = ' + this._config.schema) : Promise.resolve();
         return p.then(() => {
-            return connection.execute(sql, binding, options).then((rows) => {
-                conn.close();
+            sql = "SELECT G.NUM_0, G.TYP_0, G.ACCDAT_0, BPR_0, AMTCUR_0 FROM GACCENTRY G INNER JOIN GACCENTRYD GYD ON G.NUM_0 = GYD.NUM_0 WHERE SAC_0 = 'C7' AND GYD.SNS_0 = '-1' AND G.ACCDAT_0 >= TRUNC(sysdate - 1)";
+            return conn.execute(sql, binding, options).then((rows) => {
+                conn.release();
+                console.log("res", rows);
                 return rows;
             });
         }).catch(err => {
@@ -37,14 +36,14 @@ Pool.prototype.queryStream = function (sql, binding, options) {
     var args = arguments;
     var self = this;
     return deferred(function(str) {
-        return self.getConnection().then((conn) => {
-            var p = config.schema ? connection.execute('ALTER SESSION SET CURRENT_SCHEMA = ' + config.schema) : Promise.resolve();
+        return this._pool.getConnection().then((conn) => {
+            var p = this._config.schema ? conn.execute('ALTER SESSION SET CURRENT_SCHEMA = ' + this._config.schema) : Promise.resolve();
             p.then(() => {
-                var stream = connection.queryStream.apply(connection, args);
+                var stream = conn.queryStream.apply(conn, args);
                 closeConnection(stream);
                 stream.pipe(str);
             }).catch(err => {
-                conn.close();
+                conn.release();
                 return Promise.reject(err);
             });
         }).catch((err) => {
@@ -64,7 +63,7 @@ module.exports = function(oracledb) {
     api.createPool = function(config) {
         return oracledb.createPool(config)
         .then(function(pool) {
-            return new Pool(pool);
+            return new Pool(pool, config);
         });
     };
     
